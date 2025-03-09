f77c71d50e6993a4c8ebadc3972b830cb13fce6d
Sean Carey <wycleffsean@gmail.com>
2025-03-09 17:55:45 -0400

    env: use nix for development

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";        # Useful for multi-platform support
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        go
+      ];
+
+      shellHook = ''
+        # Set your GOPATH or GOROOT if needed, otherwise Go should work out of the box
+        # export GOPATH=$HOME/go
+        # export GOROOT=${pkgs.go}/lib/go
+        export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+        echo "Welcome to the Go development shell!"
+        exec $SHELL # use user shell
+      '';
+    };
+  });
+}
