535c876757129d69be568c04f7fbb8645a54b776
Sean Carey <wycleffsean@gmail.com>
2025-03-13 16:14:24 -0400

    lsp: enough for basic testing in an editor
    changed my personal kak/kak-lsp dotfiles and this is printing output to the debug buffer
    and is recognized by kak as an lsp for '.no' files

--- cmd/lsp.go
+++ cmd/lsp.go
@@ -1,10 +1,36 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
+
+// LogMessage is the JSON structure for LSP logging
+type LogMessage struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Params  struct {
+		Type    int    `json:"type"`
+		Message string `json:"message"`
+	} `json:"params"`
+}
+
+func sendLogMessage(message string) {
+	logMsg := LogMessage{
+		Jsonrpc: "2.0",
+		Method:  "window/logMessage",
+	}
+	logMsg.Params.Type = 3 // Info level
+	logMsg.Params.Message = message
+
+	jsonData, _ := json.Marshal(logMsg)
+	fmt.Printf("Content-Length: %d\r\n\r\n%s", len(jsonData), jsonData) // LSP message format
+}
 
 // lspCmd represents the language server command.
 var lspCmd = &cobra.Command{
@@ -12,8 +38,15 @@ var lspCmd = &cobra.Command{
 	Short: "Launch the language server for Nostos.",
 	Long:  `The lsp command starts the built-in language server, enabling features like autocompletion, diagnostics, and more in supported editors.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Starting language server...")
-		// TODO: Insert language server initialization logic here.
+		log.SetOutput(os.Stderr) // Ensure logs appear in *debug*
+		// log.SetFlags(0)          // No timestamps
+		log.Println("Nostos LSP initializing...")
+		for {
+			sendLogMessage("Hello from Nostos LSP!")
+			log.Println("Debug log to Kakoune stderr")
+
+			time.Sleep(5 * time.Second)
+		}
 	},
 }
 
