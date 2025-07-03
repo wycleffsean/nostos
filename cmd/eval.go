299973815a84a0d83ab27d2b87573ddf0ae020af
Sean Carey <wycleffsean@gmail.com>
2025-07-03 03:19:20 -0400

    Add eval command and Inspect support (#46)

---
+++ cmd/eval.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/wycleffsean/nostos/lang"
+	"github.com/wycleffsean/nostos/pkg/types"
+	"github.com/wycleffsean/nostos/vm"
+)
+
+var evalCmd = &cobra.Command{
+	Use:   "eval",
+	Short: "Evaluate NostOS code from stdin",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		data, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		_, items := lang.NewStringLexer(string(data))
+		p := lang.NewParser(items)
+		ast := p.Parse()
+		res, err := vm.Eval(ast)
+		if err != nil {
+			return err
+		}
+		fmt.Print(types.InspectValue(res))
+		return nil
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(evalCmd)
+}
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -5,20 +5,25 @@ package types
 // ---------------------------------------------------------------------------
 
 // Type is the common interface implemented by all types in the system.
-// It exposes a single Name method which should return a human readable name for
-// the type.  Primitive types will return their literal name ("string",
-// "number" etc.) while object types typically return their Kind.
-type Type interface{ Name() string }
+// It exposes a Name method which should return a human readable name for the
+// type and an Inspect method which renders a value of this type in a
+// YAML-like format.
+type Type interface {
+	Name() string
+	Inspect(v interface{}) string
+}
 
 // PrimitiveType represents a built in primitive such as string, number or bool.
 type PrimitiveType struct{ N string }
 
-func (p *PrimitiveType) Name() string { return p.N }
+func (p *PrimitiveType) Name() string                 { return p.N }
+func (p *PrimitiveType) Inspect(v interface{}) string { return InspectValue(v) }
 
 // ListType represents a list of another type.
 type ListType struct{ Elem Type }
 
-func (l *ListType) Name() string { return "[]" + l.Elem.Name() }
+func (l *ListType) Name() string                 { return "[]" + l.Elem.Name() }
+func (l *ListType) Inspect(v interface{}) string { return InspectValue(v) }
 
 // Field describes an object field.  The Type field references another Type in
 // the system which may itself be an ObjectType, ListType etc.
@@ -44,7 +49,8 @@ type ObjectType struct {
 	Open        bool
 }
 
-func (o *ObjectType) Name() string { return o.Kind }
+func (o *ObjectType) Name() string                 { return o.Kind }
+func (o *ObjectType) Inspect(v interface{}) string { return InspectValue(v) }
 
 // FunctionType represents a function that takes a list of parameter types and
 // returns another type.
@@ -53,7 +59,8 @@ type FunctionType struct {
 	Result Type
 }
 
-func (f *FunctionType) Name() string { return "func" }
+func (f *FunctionType) Name() string                 { return "func" }
+func (f *FunctionType) Inspect(v interface{}) string { return "<function>" }
 
 // Extend merges fields from the provided ObjectType into the receiver.  Fields
 // with the same name must be compatible – currently this means they have the
