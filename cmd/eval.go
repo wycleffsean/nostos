927584fb49e40a3852c0203d44f0babc4f4ccedb
Sean Carey <wycleffsean@gmail.com>
2025-07-04 16:49:24 -0400

    eval: allow file input (#52)

--- cmd/eval.go
+++ cmd/eval.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"go.lsp.dev/uri"
 
@@ -11,24 +12,47 @@ import (
 
 	"github.com/wycleffsean/nostos/lang"
 	"github.com/wycleffsean/nostos/pkg/types"
+	"github.com/wycleffsean/nostos/pkg/workspace"
 	"github.com/wycleffsean/nostos/vm"
 )
 
 var evalCmd = &cobra.Command{
-	Use:   "eval",
-	Short: "Evaluate NostOS code from stdin",
+	Use:   "eval [file]",
+	Short: "Evaluate NostOS code from stdin or a file",
+	Args:  cobra.RangeArgs(0, 1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		data, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			return err
+		var (
+			data    []byte
+			err     error
+			baseDir string
+			u       uri.URI
+		)
+
+		if len(args) > 0 {
+			path := args[0]
+			data, err = os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			baseDir = filepath.Dir(path)
+			u = uri.File(path)
+			workspace.Set(baseDir)
+		} else {
+			data, err = io.ReadAll(os.Stdin)
+			if err != nil {
+				return err
+			}
+			baseDir = workspace.Dir()
+			u = uri.URI("stdin")
 		}
+
 		_, items := lang.NewStringLexer(string(data))
-		p := lang.NewParser(items, uri.URI("stdin"))
+		p := lang.NewParser(items, u)
 		ast := p.Parse()
 		if perrs := lang.CollectParseErrors(ast); len(perrs) > 0 {
 			return perrs[0]
 		}
-		res, err := vm.EvalWithDir(ast, ".", uri.URI("stdin"))
+		res, err := vm.EvalWithDir(ast, baseDir, u)
 		if err != nil {
 			return err
 		}
