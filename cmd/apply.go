07853ed93926ecb78fdf8b12d6039a4346c43edf
Sean Carey <wycleffsean@gmail.com>
2025-07-15 16:19:44 -0400

    Add DAG planning and watch-based apply (#66)

--- cmd/apply.go
+++ cmd/apply.go
@@ -10,6 +10,10 @@ import (
 	"github.com/fatih/color"
 	"github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
+	meta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/restmapper"
@@ -33,11 +37,15 @@ var applyCmd = &cobra.Command{
 		}
 		desired := odysseyPlan.Resources
 		diff := planner.DiffResources(clusterPlan.Resources, desired)
-		plan, err := planner.BuildPlanFromDiff(diff)
+		list, err := planner.BuildPlanFromDiff(diff)
 		if err != nil {
 			return err
 		}
-		return runApply(plan)
+		dag, err := planner.BuildDAG(list)
+		if err != nil {
+			return err
+		}
+		return runApply(dag)
 	},
 }
 
@@ -45,7 +53,7 @@ func init() {
 	RootCmd.AddCommand(applyCmd)
 }
 
-func runApply(resources []planner.ResourceType) error {
+func runApply(dag *planner.DAG) error {
 	tty := isatty.IsTerminal(os.Stdout.Fd())
 	if !tty {
 		color.NoColor = true
@@ -73,9 +81,10 @@ func runApply(resources []planner.ResourceType) error {
 	}
 	mapper := restmapper.NewDiscoveryRESTMapper(groupResources)
 
-	for i, r := range resources {
+	for i, node := range dag.Order {
+		r := node.Resource
 		id := planner.ResourceID(r)
-		prefix := fmt.Sprintf("%2d/%d", i+1, len(resources))
+		prefix := fmt.Sprintf("%2d/%d", i+1, len(dag.Order))
 		if tty {
 			sp.Prefix = prefix + " "
 			sp.Suffix = " " + cyan(id)
@@ -83,7 +92,9 @@ func runApply(resources []planner.ResourceType) error {
 		} else {
 			fmt.Printf("%s applying %s\n", prefix, id)
 		}
-		err := kube.ApplyResource(context.TODO(), dyn, mapper, planner.ConvertResourceType(r))
+
+		err := applyResource(context.TODO(), dyn, mapper, r, prefix)
+
 		if tty {
 			sp.Stop()
 			if err != nil {
@@ -103,4 +114,29 @@ func runApply(resources []planner.ResourceType) error {
 		}
 	}
 	return nil
+}
+
+func applyResource(ctx context.Context, dyn dynamic.Interface, mapper meta.RESTMapper, r planner.ResourceType, prefix string) error {
+	gv, err := schema.ParseGroupVersion(r.APIVersion)
+	if err != nil {
+		return err
+	}
+	gvk := gv.WithKind(r.Kind)
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return err
+	}
+	ns, _ := r.Metadata["namespace"].(string)
+	watchCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	w, watchErr := dyn.Resource(mapping.Resource).Namespace(ns).Watch(watchCtx, metav1.ListOptions{})
+	if watchErr == nil {
+		go func(id string, watcher watch.Interface) {
+			for e := range watcher.ResultChan() {
+				fmt.Printf("%s event %s: %s\n", prefix, id, e.Type)
+			}
+		}(planner.ResourceID(r), w)
+	}
+	err = kube.ApplyResource(ctx, dyn, mapper, planner.ConvertResourceType(r))
+	cancel()
+	return err
 }
